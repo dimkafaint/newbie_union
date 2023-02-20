7464c9cedf5ada05c96389afbb2213521fd8c76a
Dmitriy <dimkafaint@gmail.com>
2023-02-20 16:39:28 +0300

    nginx_settings

--- newbie_union/newbie_union/settings.py
+++ newbie_union/newbie_union/settings.py
@@ -103,7 +103,8 @@ CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
