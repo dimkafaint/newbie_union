11d5d288ec517daa68400da3385b01c254b9a3d5
Dmitriy <dimkafaint@gmail.com>
2023-02-20 15:03:40 +0300

    add Dockerfile, compose without nginx

--- newbie_union/newbie_union/settings.py
+++ newbie_union/newbie_union/settings.py
@@ -60,11 +60,14 @@ WSGI_APPLICATION = 'newbie_union.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
