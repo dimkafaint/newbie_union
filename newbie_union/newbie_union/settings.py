bd4b4cee54f68d2303e54385fac941f10630e2f1
dimkafaint <dimkafaint@gmail.com>
2023-03-03 00:36:31 +0300

    static edit

--- newbie_union/newbie_union/settings.py
+++ newbie_union/newbie_union/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web', '194.32.248.13', 'newbie-union.fun', 'localhost', '127.0.0.1']
 
